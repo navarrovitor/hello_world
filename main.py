f14ceada06dbb3acf89fdedc56e92c89a2b408e2
vitor navarro <vitor.san.nav.nexus@gmail.com>
2020-11-17 15:34:42 -0300

    added character class

--- main.py
+++ main.py
@@ -95,6 +95,36 @@ class button:
         return False
 
 
+class player:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 10
+        self.walkCount = 0
+        self.left = False
+        self.right = False
+        self.standing = True
+
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if not self.standing:
+            if self.left:
+                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
+            elif self.right:
+                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+                self.walkCount += 1
+        else:
+            if self.right:
+                win.blit(walkRight[0], (self.x, self.y))
+            else:
+                win.blit(walkLeft[0], (self.x, self.y))
+
+
 # BOTÕES
 
 # botão para tela do jogo
